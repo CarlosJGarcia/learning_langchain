a9e945e60958fb4eabdcd990d2fd41203f025a4a
Carlos J. Garcia <your_email@example.com>
2026-07-26 17:46:52 +0200

    FAISS Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 **Instalación del entorno** \
 conda create -n langchain python=3.11 -y \
 conda activate langchain \
-conda install -c conda-forge faiss-gpu -y \
+conda install -c pytorch -c nvidia faiss-gpu -y \
 pip install -U langchain langchain-core langchain-community langgraph langsmith \
 pip install -U langchain-openai python-dotenv jupyterlab pydantic tiktoken
--- version.py
+++ version.py
@@ -1,8 +1,34 @@
 import sys
-# import torch
+import faiss
+import langchain
+# import langgraph
+# import langsmith
+# from langchain_openai import ChatOpenAI
+
+
 
 print()
 print(f"Python version: {sys.version_info.major}.{sys.version_info.minor}")
+print(f"LangChain version: {langchain.__version__}")
+# print(f"LangGraph version: {langgraph.__version__}")
+
+
+
+# Check how many CUDA-capable GPUs FAISS detects
+num_gpus = faiss.get_num_gpus()
+print(f"GPUs available for FAISS: {num_gpus}")
+
+# Quick sanity check
+if num_gpus > 0:
+    res = faiss.StandardGpuResources()  # Initialize GPU resources
+    print("FAISS GPU successfully initialized on your RTX 3090!")
+else:
+    print("CUDA support not detected.")
+
+
+
+
+
 # print("PyTorch version:", torch.__version__)
 # print("Apple Silicon acceleration:", torch.backends.mps.is_available())
 # print("CUDA enabled:", torch.cuda.is_available())
